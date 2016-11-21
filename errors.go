c175b2e2814412851c430b55198498ae0eeb57e9
Alexey Surikov <alexey.surikov@booking.com>
2016-11-21 13:17:56 +0100

    dead code

--- errors.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "errors"
-
-var (
-	errMySQLNotImplemented = errors.New("Not Implemented when using 'bolt' or 'cassandra' meta store backend")
-	errMissingParams       = errors.New("Missing params")
-)
