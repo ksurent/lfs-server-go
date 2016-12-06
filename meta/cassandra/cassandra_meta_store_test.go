aa6b48a81dd5cc9cc5f5836ecc091c44d3d9903d
Alexey Surikov <alexey.surikov@booking.com>
2016-12-06 12:20:44 +0100

    unify meta store tests: cassandra (3/3)
    this is a re-write of the Cassandra meta store tests:
            test the new Authentication method
            make sure only public interface is tested
    	less shared state between tests
            test a typical put+commit+get workflow in a single function

--- meta/cassandra/cassandra_meta_store_test.go
+++ meta/cassandra/cassandra_meta_store_test.go
@@ -1,219 +1,204 @@
 package cassandra
 
 import (
-	"errors"
 	"fmt"
 	"testing"
 
+	"github.com/ksurent/lfs-server-go/config"
 	"github.com/ksurent/lfs-server-go/meta"
 )
 
 var (
-	metaStoreTestCassandra meta.GenericMetaStore
-
-	testUser       = "admin"
-	testPass       = "admin"
-	contentSize    = int64(len("this is my content"))
-	contentOid     = "f97e1b2936a56511b3b6efc99011758e4700d60fb1674d31445d1ee40b663f24"
-	nonexistingOid = "aec070645fe53ee3b3763059376134f058cc337247c978add178b6ccdfb0019f"
-	extraRepo      = "mytestproject"
-	testRepo       = "repo"
+	testUser    = "admin"
+	testPass    = "admin"
+	contentSize = int64(len("this is my content"))
+	contentOid  = "f97e1b2936a56511b3b6efc99011758e4700d60fb1674d31445d1ee40b663f24"
+	contentRepo = "repo"
 )
 
-func TestCassandraGet(t *testing.T) {
-	serr := setupCassandraMeta()
-	if serr != nil {
-		t.Errorf(serr.Error())
-	}
-	defer teardownCassandraMeta()
-
-	m, err := metaStoreTestCassandra.Get(&meta.RequestVars{Oid: nonexistingOid})
-	if err == nil {
-		t.Fatalf("expected get to fail with unknown oid, got: %s", m.Oid)
-	}
-
-	m, err = metaStoreTestCassandra.Get(&meta.RequestVars{Oid: contentOid})
+func TestPutGet(t *testing.T) {
+	testMetaStore, err := setupMeta()
 	if err != nil {
-		t.Fatalf("expected get to succeed, got: %s", err)
+		t.Fatal(err)
+	}
+	defer teardownMeta(testMetaStore)
+
+	rv := &meta.RequestVars{
+		Oid:  contentOid,
+		Size: contentSize,
+		Repo: contentRepo,
 	}
 
-	if m.Oid != contentOid {
-		t.Errorf("expected to get content oid, got: %s", m.Oid)
+	if _, err := testMetaStore.Put(rv); err != nil {
+		t.Errorf("expected Put() to succeed, got: %s", err)
 	}
 
-	if m.Size != contentSize {
-		t.Errorf("expected to get content size, got: %d", m.Size)
+	if _, err := testMetaStore.Get(rv); !meta.IsObjectNotFound(err) {
+		t.Errorf("expected Get() to return 'not found', got: %s", err)
 	}
-}
 
-func TestCassandraUsers(t *testing.T) {
-	serr := setupCassandraMeta()
-	if serr != nil {
-		t.Errorf(serr.Error())
-	}
-	defer teardownCassandraMeta()
-
-	err := metaStoreTestCassandra.AddUser(testUser, testPass)
+	m, err := testMetaStore.GetPending(rv)
 	if err != nil {
-		t.Errorf("Unable to add user, error %s", err.Error())
-	}
-
-	users, err := metaStoreTestCassandra.Users()
-	if err != nil {
-		t.Errorf("Unable to retrieve users, error %s", err.Error())
-	}
-	if len(users) == 0 {
-		t.Errorf("Adding a user failed")
-	}
-
-	uErr := metaStoreTestCassandra.DeleteUser(testUser)
-	if uErr != nil {
-		t.Errorf("Unable to delete user, error %s", err.Error())
-	}
-}
-
-func TestCassandraPut(t *testing.T) {
-	serr := setupCassandraMeta()
-	if serr != nil {
-		t.Errorf(serr.Error())
-	}
-	defer teardownCassandraMeta()
-
-	m, err := metaStoreTestCassandra.Put(&meta.RequestVars{Oid: nonexistingOid, Size: 42})
-	if err != nil {
-		t.Errorf("expected put to succeed, got: %s", err)
-	}
-
-	if m.Existing {
-		t.Errorf("expected meta to not have existed")
-	}
-
-	_, err = metaStoreTestCassandra.Get(&meta.RequestVars{Oid: nonexistingOid})
-	if err == nil {
-		t.Errorf("expected new put to not be committed yet")
-	}
-
-	m, err = metaStoreTestCassandra.GetPending(&meta.RequestVars{Oid: nonexistingOid})
-	if err != nil {
-		t.Errorf("expected new put to be pending, got: %s", err)
-	}
-
-	if m.Oid != nonexistingOid {
-		t.Errorf("expected oids to match, got: %s", m.Oid)
-	}
-
-	if m.Size != 42 {
-		t.Errorf("expected sizes to match, got: %d", m.Size)
-	}
-
-	m, err = metaStoreTestCassandra.Commit(&meta.RequestVars{Oid: nonexistingOid, Size: 42})
-	if err != nil {
-		t.Errorf("expected commit to succeed, got: %s", err)
-	}
-
-	if !m.Existing {
-		t.Errorf("expected existing to become true after commit")
-	}
-
-	_, err = metaStoreTestCassandra.Get(&meta.RequestVars{Oid: nonexistingOid})
-	if err != nil {
-		t.Errorf("expected new put to be committed now, got: %s", err)
-	}
-
-	if !m.Existing {
-		t.Errorf("expected existing to be true for a committed object")
-	}
-
-	m, err = metaStoreTestCassandra.Put(&meta.RequestVars{Oid: nonexistingOid, Size: 42})
-	if err != nil {
-		t.Errorf("expected putting an duplicate object to succeed, got: %s", err)
-	}
-
-	if !m.Existing {
-		t.Errorf("expecting existing to be true for a duplicate object")
-	}
-}
-
-func TestCassandraProjects(t *testing.T) {
-	serr := setupCassandraMeta()
-	if serr != nil {
-		t.Errorf(serr.Error())
-	}
-	defer teardownCassandraMeta()
-
-	err := metaStoreTestCassandra.AddProject(extraRepo)
-	if err != nil {
-		t.Errorf("Failed to create project: %s", err)
-	}
-
-	listProjects, err := metaStoreTestCassandra.Projects()
-	if err != nil {
-		t.Errorf("Failed getting cassandra projects: %s", err)
-	}
-	found := false
-	for i := range listProjects {
-		if listProjects[i].Name == extraRepo {
-			found = true
+		t.Fatalf("expected GetPending() to succeed, got: %s", err)
+	} else {
+		if m.Oid != contentOid {
+			t.Errorf("expected pending object id to be %d, got: %d", contentOid, m.Oid)
+		}
+		if m.Size != contentSize {
+			t.Errorf("expected pending object size to be %d, got: %d", contentSize, m.Size)
+		}
+		if m.Existing {
+			t.Error("expected meta object to be in the pending state")
+		}
+		if len(m.ProjectNames) != 1 || m.ProjectNames[0] != contentRepo {
+			t.Errorf("expected pending object to belong to project %q, got: %v", contentRepo, m.ProjectNames)
 		}
 	}
-	if !found {
-		t.Errorf("Failed finding project %s", extraRepo)
+
+	if _, err := testMetaStore.Commit(rv); err != nil {
+		t.Errorf("expected Commit() to succeed, got: %s", err)
+	}
+
+	if m, err = testMetaStore.GetPending(rv); !meta.IsObjectNotFound(err) {
+		t.Errorf("expected GetPending() to return 'not found', got: %s %s", err, m)
+	}
+
+	m, err = testMetaStore.Get(rv)
+	if err != nil {
+		t.Errorf("expected Get() to succeed, got: %s", err)
+	} else {
+		if m.Oid != contentOid {
+			t.Errorf("expected committed object id to be %d, got: %d", contentOid, m.Oid)
+		}
+		if m.Size != contentSize {
+			t.Errorf("expected committed object size to be %d, got: %d", contentSize, m.Size)
+		}
+		if !m.Existing {
+			t.Error("expected meta object to be in the committed state")
+		}
+		if len(m.ProjectNames) != 1 || m.ProjectNames[0] != contentRepo {
+			t.Errorf("expected committed object to belong to project %q, got: %v", contentRepo, m.ProjectNames)
+		}
 	}
 }
 
-func TestCassandraAuthenticate(t *testing.T) {
-	serr := setupCassandraMeta()
-	if serr != nil {
-		t.Errorf(serr.Error())
+func TestPutDuplicate(t *testing.T) {
+	testMetaStore, err := setupMeta()
+	if err != nil {
+		t.Fatal(err)
 	}
-	defer teardownCassandraMeta()
+	defer teardownMeta(testMetaStore)
 
-	ok, _ := metaStoreTestCassandra.Authenticate(testUser, testPass)
+	rv := &meta.RequestVars{
+		Oid:  contentOid,
+		Size: contentSize,
+		Repo: contentRepo,
+	}
+
+	_, err = testMetaStore.Put(rv)
+	if err != nil {
+		t.Errorf("expected Put() to succeed, got: %s", err)
+	}
+
+	_, err = testMetaStore.Put(rv)
+	if err != nil {
+		t.Errorf("expected duplicate pending Put() to succeed, got: %s", err)
+	}
+
+	if _, err = testMetaStore.Commit(rv); err != nil {
+		t.Errorf("expected Commit() to succeed, got: %s", err)
+	}
+
+	_, err = testMetaStore.Put(rv)
+	if err != nil {
+		t.Errorf("expected duplicate committed Put() to succeed, got: %s", err)
+	}
+}
+
+func TestProjects(t *testing.T) {
+	testMetaStore, err := setupMeta()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer teardownMeta(testMetaStore)
+
+	if err := testMetaStore.AddProject(contentRepo); err != nil {
+		t.Errorf("expected AddProject() to succeed, got: %s", err)
+	}
+
+	projects, err := testMetaStore.Projects()
+	if err != nil {
+		t.Errorf("expected Projects() to succeed, got: %s", err)
+	} else if len(projects) != 1 || projects[0].Name != contentRepo {
+		t.Errorf("expected Projects() to return %s, got: %v", contentRepo, projects)
+	}
+}
+
+func TestAuthentication(t *testing.T) {
+	testMetaStore, err := setupMeta()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer teardownMeta(testMetaStore)
+
+	if err := testMetaStore.AddUser(testUser, testPass); err != nil {
+		t.Errorf("expected AddUser() to succeed, got: %s", err)
+	}
+
+	users, err := testMetaStore.Users()
+	if err != nil {
+		t.Errorf("expected Users() to succeed, got: %s", err)
+	} else if len(users) != 1 || users[0].Name != testUser {
+		t.Errorf("expected Users() to return %s, got: %v", testUser, users)
+	}
+
+	ok, err := testMetaStore.Authenticate(testUser, testPass)
 	if !ok {
-		t.Errorf("expected authentication to succeed")
+		if err != nil {
+			t.Errorf("expected Authenticate() to succeed, got: %s", err)
+		} else {
+			t.Error("expected Authenticate() to succeed")
+		}
 	}
 
-	ok, _ = metaStoreTestCassandra.Authenticate("azog", "defiler")
+	if err := testMetaStore.DeleteUser(testUser); err != nil {
+		t.Errorf("expected DeleteUser() to succeed, got: %s", err)
+	}
+
+	users, err = testMetaStore.Users()
+	if err != nil {
+		t.Errorf("expected Users() to succeed, got: %s", err)
+	} else if len(users) != 0 {
+		t.Errorf("expected Users() to not return deleted user, got: %v", users)
+	}
+
+	ok, _ = testMetaStore.Authenticate(testUser, testPass)
 	if ok {
-		t.Errorf("expected authentication to fail")
+		t.Errorf("expected Authenticate() to fail for a deleted user")
+	}
+
+	ok, _ = testMetaStore.Authenticate("azog", "defiler")
+	if ok {
+		t.Errorf("expected Authenticate() to fail for nonexisting user")
 	}
 }
 
-func setupCassandraMeta() error {
-	store, err := NewCassandraMetaStore()
+func setupMeta() (*CassandraMetaStore, error) {
+	metaStore, err := NewCassandraMetaStore()
 	if err != nil {
-		fmt.Printf("error initializing test meta store: %s\n", err)
-		return errors.New(fmt.Sprintf("error initializing test meta store: %s\n", err))
+		return nil, err
 	}
 
-	metaStoreTestCassandra = store
-	if err := metaStoreTestCassandra.AddUser(testUser, testPass); err != nil {
-		teardownCassandraMeta()
-		fmt.Printf("error adding test user to meta store: %s\n", err)
-		return errors.New(fmt.Sprintf("error adding test user to meta store: %s\n", err))
-	}
+	metaStore.client.Query("TRUNCATE TABLE oids").Exec()
+	metaStore.client.Query("TRUNCATE TABLE projects").Exec()
+	metaStore.client.Query("TRUNCATE TABLE users").Exec()
 
-	rv := &meta.RequestVars{Oid: contentOid, Size: contentSize, Repo: testRepo}
-
-	if _, err := metaStoreTestCassandra.Put(rv); err != nil {
-		teardownCassandraMeta()
-		fmt.Printf("error seeding cassandra test meta store: %s\n", err)
-		return errors.New(fmt.Sprintf("error seeding cassandra test meta store: %s\n", err))
-	}
-	if _, err := metaStoreTestCassandra.Commit(rv); err != nil {
-		teardownCassandraMeta()
-		fmt.Printf("error seeding cassandra test meta store: %s\n", err)
-		return errors.New(fmt.Sprintf("error seeding cassandra test meta store: %s\n", err))
-	}
-
-	return nil
+	return metaStore, nil
 }
 
-func teardownCassandraMeta() {
-	sess, err := NewCassandraSession()
-	if err != nil {
-		fmt.Printf("error tearing down cassandra test meta store: %s\n", err)
-		return
-	}
-	DropCassandra(sess.Client)
+func teardownMeta(metaStore *CassandraMetaStore) {
+	ks := fmt.Sprintf("%s_%s", config.Config.Cassandra.Keyspace, config.GoEnv)
+	metaStore.client.Query(fmt.Sprintf("drop keyspace %s;", ks)).Exec()
+
+	metaStore.Close()
 }
